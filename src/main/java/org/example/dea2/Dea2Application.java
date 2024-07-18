f08668414df684c2a5e7817bc97f8ac04a7fff7f
ishu6285 <isharaumesh2000@gmail.com>
2024-07-18 10:52:26 +0530

    create spring project

---
+++ src/main/java/org/example/dea2/Dea2Application.java
@@ -0,0 +1,13 @@
+package org.example.dea2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dea2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dea2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/dea2/Dea2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.dea2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dea2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
